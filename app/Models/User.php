19207d0e285970835d0de7c2a2b537e0c8895bae
dinncer <dinncer@gmail.com>
2019-01-29 19:07:22 +0300

    User model SoftDeletes feature added

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
 {
     use Notifiable;
+
+    use SoftDeletes;    
 
     /**
      * The attributes that are mass assignable.
